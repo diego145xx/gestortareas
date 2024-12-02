7b584f79226d019dad862c671f5e61329416d165
diego145xx <dracoxbox2@gmail.com>
2024-12-02 18:01:18 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <!-- Barra de navegación con enlaces a diferentes rutas -->
+    <nav>
+      <ul class="nav">
+  <li class="nav-item">
+    <router-link to="/">Home</router-link> |
+  </li>
+  <li class="nav-item">
+    <router-link to="/addtask">Agregar Tarea</router-link> 
+  </li>
+  <li class="nav-item">
+    <router-link to="/tasklist">Lista de Tareas</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link to="/combinedview">Vista Combinada</router-link>
+  </li>
+</ul>
+    </nav>
+    <!-- Aquí se cargará el componente correspondiente a la ruta seleccionada -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Esta aplicación te permite gestionar tus tareas de manera eficiente. Puedes añadir nuevas tareas manualmente o extraerlas desde una API.
+    </p>
+    <h3>Secciones de la Aplicación</h3>
+    
+    <h3>Lista de Tareas: Muestra todas las tareas extraídas desde la API, permitiendo marcarlas como completadas o eliminarlas. </h3>
+    <ul>
+  
+    </ul>
+    <h3>Añadir Tarea: Permite ingresar nuevas tareas manualmente.</h3>
+    <ul>
+      
+      Vista Combinada: Combina las funcionalidades de añadir tareas manualmente y ver las tareas de la API al mismo tiempo
+     
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
